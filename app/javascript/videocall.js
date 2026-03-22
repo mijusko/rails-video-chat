bacf6d1d2c643874b4a4b55c3ac28137efee65cf
mijusko <mihajlo00kg@gmail.com>
2026-03-22 14:25:18 +0100

    Add TURN servers

--- app/javascript/videocall.js
+++ app/javascript/videocall.js
@@ -37,7 +37,23 @@ document.addEventListener("turbo:load", () => {
   const ICE_SERVERS = {
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
-      { urls: 'stun:stun1.l.google.com:19302' }
+      { urls: 'stun:stun1.l.google.com:19302' },
+      // Free public TURN servers to handle strict NATs (different networks/4G)
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
     ]
   }
 
